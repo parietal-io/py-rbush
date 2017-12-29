c12630ea1a936aa839a4b80e7b9c8b0702254f82
bcollins <parietal.io@gmail.com>
2017-12-29 14:45:16 -0600

    added brute force numba test

---
+++ rbush/tests/test_brute_force.py
@@ -0,0 +1,46 @@
+from rbush import RBush 
+from rbush.data import generate_data_array
+from time import time
+import numpy as np
+
+import numba as nb
+
+def test_against_brute_force_numba():
+    b = RBush()
+    data = generate_data_array(100000,10)
+    b.load(data)
+
+    search_box = (-1, -1, 1, 1)
+
+    c = search_brute_force(b.data, b.boxes, *search_box)
+    t1 = time()
+    c = search_brute_force(b.data, b.boxes, *search_box)
+    c = search_brute_force(b.data, b.boxes, *search_box)
+    c = search_brute_force(b.data, b.boxes, *search_box)
+    t2 = time()
+    print('BRUTE FORCE;', len(c), 'time: {:.5f}'.format(t2 - t1))
+
+    c, d = b.search(*search_box)
+    rbush_t1 = time()
+    c, d = b.search(*search_box)
+    c, d = b.search(*search_box)
+    c, d = b.search(*search_box)
+    rbush_t2 = time()
+    print('RBUSH;', len(c), 'time: {:.5f}'.format(rbush_t2 - rbush_t1))
+    assert rbush_t2 - rbush_t1 < t2 - t1, 'Sorry not fast enough yet'
+
+
+@nb.njit
+def search_brute_force(data, boxes, xmin, ymin, xmax, ymax):
+    final_result = []
+    for i in range(len(boxes)):
+        rxmin = boxes[i][0]
+        rymin = boxes[i][1]
+        rxmax = boxes[i][2]
+        rymax = boxes[i][3]
+
+        # intersects 
+        if (rxmin <= xmax and rymin <= ymax) and (rxmax >= xmin and rymax >= ymin):
+            final_result.append(data[i])
+
+    return final_result
