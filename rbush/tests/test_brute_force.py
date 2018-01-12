7ffbddf1e4b313b0103d5ba93a8def178e9da015
Carlos Brandt <chbrandt@pc>
2018-01-12 21:59:15 +0100

    Update brute_force to have the test.assert on time

--- rbush/tests/test_brute_force.py
+++ rbush/tests/test_brute_force.py
@@ -1,4 +1,4 @@
-from rbush import RBush
+from rbush.rbush_core import RBush
 from rbush.data import generate_data_array
 from time import time
 import numpy as np
@@ -19,7 +19,8 @@ def test_against_brute_force_numba():
     c = search_brute_force(b.data, b.boxes, *search_box)
     c = search_brute_force(b.data, b.boxes, *search_box)
     t2 = time()
-    print('BRUTE FORCE;', len(c), 'time: {:.5f}'.format(t2 - t1))
+    t_brute = t2 - t1
+    print('BRUTE FORCE;', len(c), 'time: {:.5f}'.format(t_brute))
 
     c = b.search(*search_box)
     rbush_t1 = time()
@@ -27,7 +28,10 @@ def test_against_brute_force_numba():
     c = b.search(*search_box)
     c = b.search(*search_box)
     rbush_t2 = time()
-    print('RBUSH;', len(c), 'time: {:.5f}'.format(rbush_t2 - rbush_t1))
+    #print('RBUSH;', len(c), 'time: {:.5f}'.format(rbush_t2 - rbush_t1))
+    t_goal = rbush_t2 - rbush_t1
+    print('RBUSH;', c.size, 'time: {:.5f}'.format(t_goal))
+    assert t_goal < t_brute, "Nope...not fast enough"
 
 
 @nb.jit
